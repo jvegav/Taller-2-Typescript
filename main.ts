43e75556782e1fc402a88ab384a3bec2ae781aef
Josue Vega <j.vegav@uniandes.edu.co>
2023-10-22 19:32:05 -0500

    taller 2 final

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@ import { series } from './data.js';
 let seriesTbody: HTMLElement = document.getElementById('series')!; // Nodo tbody que tiene el id="series"
 const promSeasons: HTMLElement = document.getElementById("promedio-seasons")!;
 
+const seriesDetail: HTMLElement = document.getElementById("series-detail")!;
+
 
 
 renderSeriesInTable(series);
@@ -22,8 +24,24 @@ function renderSeriesInTable(series: Serie[]): void {
                            <td>${c.channel}</td>
                            <td>${c.seasons}</td>
                            `;
+    trElement.addEventListener("click", () => {
+                            showSeriesDetail(c);
+              });
     seriesTbody.appendChild(trElement);
-  });
+  })
+}
+
+
+function showSeriesDetail(serie : Serie) {
+  
+  seriesDetail.innerHTML = `
+      <div class="card-body">
+          <img src = ${serie.img}></img>
+          <br>
+          <h5 class="card-title">${serie.name}</h5>
+          <p class="card-text">${serie.description}</p>
+      </div>
+  `;
 }
 
 function getSeasons(series: Serie[]): number {
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,21 @@
 import { series } from './data.js';
 var seriesTbody = document.getElementById('series'); // Nodo tbody que tiene el id="series"
 var promSeasons = document.getElementById("promedio-seasons");
+var seriesDetail = document.getElementById("series-detail");
 renderSeriesInTable(series);
 promSeasons.innerHTML = "".concat(getSeasons(series));
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
         trElement.innerHTML = "<td>".concat(c.top, "</td>\n                           <td>").concat(c.name, "</td>\n                           <td>").concat(c.channel, "</td>\n                           <td>").concat(c.seasons, "</td>\n                           ");
+        trElement.addEventListener("click", function () {
+            showSeriesDetail(c);
+        });
         seriesTbody.appendChild(trElement);
     });
+}
+function showSeriesDetail(serie) {
+    seriesDetail.innerHTML = "\n      <div class=\"card-body\">\n          <img src = ".concat(serie.img, "></img>\n          <h5 class=\"card-title\">").concat(serie.name, "</h5>\n          <p class=\"card-text\">").concat(serie.description, "</p>\n      </div>\n  ");
 }
 function getSeasons(series) {
     var promSeasons = 0;
